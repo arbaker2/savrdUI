b699ffce02e09074427fa685ff74908fac0ef939
Adam Baker <adambaker@Adams-MacBook-Pro.local>
2024-06-28 17:41:53 -0500

    fix(post_view): remove un-used widget

--- lib/src/post/post_view.dart
+++ lib/src/post/post_view.dart
@@ -19,177 +19,168 @@ class PostView extends StatelessWidget {
   Widget build(BuildContext context) {
     Post postLocal = post;
     bool isRecipe = postLocal is RecipePost;
-    return DefaultTabController(
-      initialIndex: 0,
-      length: 2,
-      animationDuration: const Duration(milliseconds: 600),
-      child: Scaffold(
-        body: SafeArea(
-          child: NestedScrollView(
-            headerSliverBuilder: buildHeaderSliverBuilder(),
-            body: Padding(
-              padding: const EdgeInsets.all(16.0),
-              child: SingleChildScrollView(
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    ImageTile(
-                      postLocal.imageAssetLocation,
+    return Scaffold(
+      body: SafeArea(
+        child: NestedScrollView(
+          headerSliverBuilder: buildHeaderSliverBuilder(),
+          body: Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: SingleChildScrollView(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  ImageTile(
+                    postLocal.imageAssetLocation,
+                  ),
+                  // FIXME: refactor post lable used in post tile
+                  Padding(
+                    padding: const EdgeInsets.only(top: 20.0),
+                    child: Text(
+                      postLocal.title,
+                      style: Theme.of(context).textTheme.headlineSmall,
                     ),
-                    // FIXME: refactor post lable used in post tile
-                    Padding(
-                      padding: const EdgeInsets.only(top: 20.0),
-                      child: Text(
-                        postLocal.title,
-                        style: Theme.of(context).textTheme.headlineSmall,
-                      ),
-                    ),
-                    if (isRecipe)
-                      IntrinsicHeight(
-                          child: Row(
-                        children: [
-                          Text(postLocal.time.toString()),
-                          const SizedBox(
-                            width: 2,
-                          ),
-                          const FaIcon(
-                            FontAwesomeIcons.clock,
-                            size: 15,
-                          ),
-                          const VerticalDivider(
+                  ),
+                  if (isRecipe)
+                    IntrinsicHeight(
+                        child: Row(
+                      children: [
+                        Text(postLocal.time.toString()),
+                        const SizedBox(
+                          width: 2,
+                        ),
+                        const FaIcon(
+                          FontAwesomeIcons.clock,
+                          size: 15,
+                        ),
+                        const VerticalDivider(
+                          indent: 3,
+                          endIndent: 3,
+                          thickness: 1.5,
+                        ),
+                        Text(postLocal.servings),
+                        const SizedBox(
+                          width: 2,
+                        ),
+                        const FaIcon(
+                          FontAwesomeIcons.userGroup,
+                          size: 15,
+                        ),
+                      ],
+                    )),
+                  // generate tag widgets from list of strings
+                  if (isRecipe)
+                    IntrinsicHeight(
+                      child: Row(children: [
+                        ...List.generate(postLocal.tags.length * 2 - 1, (item) {
+                          if (item.isEven) {
+                            return Text(postLocal.tags[item ~/ 2]);
+                          }
+                          return const VerticalDivider(
                             indent: 3,
                             endIndent: 3,
                             thickness: 1.5,
-                          ),
-                          Text(postLocal.servings),
-                          const SizedBox(
-                            width: 2,
-                          ),
-                          const FaIcon(
-                            FontAwesomeIcons.userGroup,
-                            size: 15,
-                          ),
-                        ],
-                      )),
-                    // generate tag widgets from list of strings
-                    if (isRecipe)
-                      IntrinsicHeight(
-                        child: Row(children: [
-                          ...List.generate(postLocal.tags.length * 2 - 1,
-                              (item) {
-                            if (item.isEven) {
-                              return Text(postLocal.tags[item ~/ 2]);
-                            }
-                            return const VerticalDivider(
-                              indent: 3,
-                              endIndent: 3,
-                              thickness: 1.5,
-                            );
-                          }),
-                        ]),
-                      ),
-                    // FIXME: Refactor the two below widgets
-                    if (isRecipe)
-                      Text(
-                        postLocal.caption,
-                        style: Theme.of(context).textTheme.bodySmall,
-                      ),
-                    // FIXME: group
-                    if (isRecipe)
-                      if (postLocal.blog != null)
-                        Section(
-                          child: Text(
-                            postLocal.blog!,
-                            style: Theme.of(context).textTheme.bodySmall,
-                          ),
-                        ),
-
-                    if (isRecipe)
+                          );
+                        }),
+                      ]),
+                    ),
+                  // FIXME: Refactor the two below widgets
+                  if (isRecipe)
+                    Text(
+                      postLocal.caption,
+                      style: Theme.of(context).textTheme.bodySmall,
+                    ),
+                  // FIXME: group
+                  if (isRecipe)
+                    if (postLocal.blog != null)
                       Section(
-                        child: Card(
-                          child: Padding(
-                            padding: const EdgeInsets.all(8.0),
-                            child: Row(
-                              children: [
-                                Column(
-                                  crossAxisAlignment: CrossAxisAlignment.start,
-                                  children: <Widget>[
-                                    ...List.generate(
-                                      postLocal.ingredients.length,
-                                      (int index) {
-                                        final ingredient =
-                                            postLocal.ingredients[index];
-
-                                        return RichText(
-                                          text: TextSpan(
-                                            text: ingredient.amount,
-                                            style: Theme.of(context)
-                                                .textTheme
-                                                .bodyLarge,
-                                            children: [
-                                              TextSpan(
-                                                text: ingredient.item,
-                                                style: Theme.of(context)
-                                                    .textTheme
-                                                    .bodyMedium,
-                                              )
-                                            ],
-                                          ),
-                                        );
-                                      },
-                                      growable: false,
-                                    ),
-                                  ],
-                                ),
-                              ],
-                            ),
+                        child: Text(
+                          postLocal.blog!,
+                          style: Theme.of(context).textTheme.bodySmall,
+                        ),
+                      ),
+                  if (isRecipe)
+                    Section(
+                      child: Card(
+                        child: Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: Row(
+                            children: [
+                              Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: <Widget>[
+                                  ...List.generate(
+                                    postLocal.ingredients.length,
+                                    (int index) {
+                                      final ingredient =
+                                          postLocal.ingredients[index];
+                                      return RichText(
+                                        text: TextSpan(
+                                          text: ingredient.amount,
+                                          style: Theme.of(context)
+                                              .textTheme
+                                              .bodyLarge,
+                                          children: [
+                                            TextSpan(
+                                              text: ingredient.item,
+                                              style: Theme.of(context)
+                                                  .textTheme
+                                                  .bodyMedium,
+                                            )
+                                          ],
+                                        ),
+                                      );
+                                    },
+                                    growable: false,
+                                  ),
+                                ],
+                              ),
+                            ],
                           ),
                         ),
                       ),
-                    if (isRecipe)
-                      Section(
-                        child: Column(
-                          children: <Widget>[
-                            ...List.generate(
-                              postLocal.directions.length,
-                              (int index) {
-                                final direction = postLocal.directions[index];
-                                return Card(
-                                  child: ListTile(
-                                    title: RichText(
-                                      text: TextSpan(
-                                        text: "${index + 1} ",
-                                        style: Theme.of(context)
-                                            .textTheme
-                                            .bodyLarge,
-                                        children: [
-                                          TextSpan(
-                                            text: direction,
-                                            style: Theme.of(context)
-                                                .textTheme
-                                                .bodyMedium,
-                                          ),
-                                        ],
-                                      ),
+                    ),
+                  if (isRecipe)
+                    Section(
+                      child: Column(
+                        children: <Widget>[
+                          ...List.generate(
+                            postLocal.directions.length,
+                            (int index) {
+                              final direction = postLocal.directions[index];
+                              return Card(
+                                child: ListTile(
+                                  title: RichText(
+                                    text: TextSpan(
+                                      text: "${index + 1} ",
+                                      style:
+                                          Theme.of(context).textTheme.bodyLarge,
+                                      children: [
+                                        TextSpan(
+                                          text: direction,
+                                          style: Theme.of(context)
+                                              .textTheme
+                                              .bodyMedium,
+                                        ),
+                                      ],
                                     ),
                                   ),
-                                );
-                              },
-                              growable: false,
-                            ),
-                          ],
+                                ),
+                              );
+                            },
+                            growable: false,
+                          ),
+                        ],
+                      ),
+                    ),
+                  if (isRecipe)
+                    if (postLocal.notes != null)
+                      Section(
+                        child: Text(
+                          postLocal.notes!,
+                          style: Theme.of(context).textTheme.bodySmall,
                         ),
                       ),
-                    if (isRecipe)
-                      if (postLocal.notes != null)
-                        Section(
-                          child: Text(
-                            postLocal.notes!,
-                            style: Theme.of(context).textTheme.bodySmall,
-                          ),
-                        ),
-                  ],
-                ),
+                ],
               ),
             ),
           ),
